4e525a0262c51904ed3323d628b0e7bd7e00b089
Dimets <dimets1977@gmail.com>
2022-03-28 15:03:41 +0300

    fix 1. классы Node & TaskLinkedList параметризовать типом Т, а не Task 2. проверить наличие таски в мапе перед удалением ноды 3. не хранить размер списка в TaskLinkedList (не использовать в алгоритме удаления ноды)

--- src/history/InMemoryHistoryManager.java
+++ src/history/InMemoryHistoryManager.java
@@ -8,14 +8,13 @@ public class InMemoryHistoryManager implements HistoryManager {
     private Map<Integer,Node<Task>> historyMap = new HashMap<>();
     private TaskLinkedList<Task> taskTaskLinkedList = new TaskLinkedList<>();
 
-    class TaskLinkedList<Task> {
-        private Node<Task> headNode;
-        private Node<Task> tailNode;
-        private int size = 0;
+   private class TaskLinkedList<T> {
+        private Node<T> headNode;
+        private Node<T> tailNode;
 
-        public Node<Task> linkLast(Task task) {
-            Node<Task> lastNode = tailNode;
-            Node<Task> newNode = new Node<>(tailNode, null, task);
+        public Node<T> linkLast(T task) {
+            Node<T> lastNode = tailNode;
+            Node<T> newNode = new Node<>(tailNode, null, task);
 
             tailNode = newNode;
 
@@ -24,48 +23,41 @@ public class InMemoryHistoryManager implements HistoryManager {
             } else {
                 lastNode.setNext(newNode);
             }
-            size++;
 
             return newNode;
         }
 
-        public List<Task> getTasks() {
-            List<Task> historyList = new ArrayList<>();
+        public List<T> getTasks() {
+            List<T> historyList = new ArrayList<>();
 
-            for (Node<Task> i = headNode; i != null; i = i.getNext()) {
+            for (Node<T> i = headNode; i != null; i = i.getNext()) {
                 historyList.add(i.getData());
             }
 
             return historyList;
         }
 
-        public void removeNode(Node<Task> node) {
-            Node<Task> prev = node.getPrev();
-            Node<Task> next = node.getNext();
+        public void removeNode(Node<T> node) {
+            Node<T> prev = node.getPrev();
+            Node<T> next = node.getNext();
 
-            if (size == 1) {
-                headNode = null;
-                tailNode = null;
-                clearNode(node);
-            } else if (size > 1) {
-                if (node == headNode) {
-                    headNode = next;
-                    next.setPrev(null);
-                    clearNode(node);
-                } else if (node == tailNode) {
-                    prev.setNext(null);
-                    tailNode = prev;
-                    clearNode(node);
-                } else {
-                    next.setPrev(prev);
-                    prev.setNext(next);
-                    clearNode(node);
-                }
-            }
+           if (node == headNode) {
+               headNode = next;
+               next.setPrev(null);
+               clearNode(node);
+           } else if (node == tailNode) {
+               prev.setNext(null);
+               tailNode = prev;
+               clearNode(node);
+           } else {
+               next.setPrev(prev);
+               prev.setNext(next);
+               clearNode(node);
+           }
+        }
 
-            }
 
-        private void clearNode(Node<Task> node) {
+        private void clearNode(Node<T> node) {
             node.setPrev(null);
             node.setNext(null);
             node.setData(null);
@@ -87,8 +79,10 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void remove(int id) {
-        taskTaskLinkedList.removeNode(historyMap.get(id));
-        historyMap.remove(id);
+        if (historyMap.containsKey(id)) {
+            taskTaskLinkedList.removeNode(historyMap.get(id));
+            historyMap.remove(id);
+        }
     }
 
     @Override
